a2c0411bbdf04c6d62b1a6d36c6bce3371d6f6bb
hridik-suresh <iamhridiksuresh@gmail.com>
2025-07-31 16:07:55 +0530

    some new features were added

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ document.addEventListener("DOMContentLoaded",() =>{
     const taskList = document.querySelector('#task-list');
     const emptyImg = document.querySelector('.empty-img');
     const todocontainer = document.querySelector('.todo-container');
-
     
-    const addTask = (evt) =>  {
+    //text for test
+    const addTask = (evt, text, completed=false) =>  {  
+
         evt.preventDefault();
 
-        const taskText = taskInput.value.trim();
+        const taskText = text || taskInput.value.trim();
         if (!taskText){
             return;
         };
 
         const li = document.createElement('li');
         li.innerHTML= `
-        <input type='checkbox' class="checkbox">
+        <input type='checkbox' class="checkbox" ${completed ? 'checked':''} >
         <span>${taskText}</span>
         <div class="task-buttons">
             <button class="edit-btn">
@@ -30,6 +31,23 @@ document.addEventListener("DOMContentLoaded",() =>{
 
         const editBtn = li.querySelector('.edit-btn');
         const checkbox = li.querySelector('.checkbox');
+
+        //For test-----
+        if(completed){ 
+            li.classList.add('completed');
+            editBtn.disabled = true;
+            editBtn.style.opacity = "0.5";
+            editBtn.style.pointerEvents = "none";
+        }
+
+        checkbox.addEventListener('change', ()=>{
+            const isChecked = checkbox.checked  //return true or false
+            li.classList.toggle('completed', isChecked);
+            editBtn.disabled = isChecked;
+            editBtn.style.opacity = isChecked ? '0.5':'1';
+            editBtn.style.pointerEvents = isChecked ? 'none':'auto';
+        })
+
         editBtn.addEventListener("click", () =>{
             if(!checkbox.checked){
                 taskInput.value = li.querySelector("span").innerText;
@@ -50,15 +68,12 @@ document.addEventListener("DOMContentLoaded",() =>{
         toggleEmptyState()
     }
 
-
-
-    addTaskBtn.addEventListener("click", addTask);
-
-    taskInput.addEventListener("keypress", (e) =>{
-        if(e.key === "enter"){
+    addTaskBtn.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', (e) =>{
+        if(e.key == 'Enter'){
             addTask(e);
         }
-    })
+    });
 
     function toggleEmptyState(){
         emptyImg.style.display = taskList.children.length === 0 ? "block" : "none";
